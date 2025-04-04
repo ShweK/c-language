621f4ba3e903b9cf619697fb59bc4a267ef4d841
Shweta Sasane <51405426+ShweK@users.noreply.github.com>
2025-04-04 21:07:39 +0000

    "add the comments for floating point encoding-float and double"

--- dataTypes/c-asm.c
+++ dataTypes/c-asm.c
@@ -1,6 +1,11 @@
 int decode(){
 
     int i = -1;
-     
-    return i;
+    float rand_val = 2.5; //floating encoding uses the IEEE 754 standard, 1bit signed, 8bit exponent, 23 bit mantessa and bias value is 127== 32 bit(4bytes)
+    double val = 2.5; // double precision uses the IEEE 754 std, 1 bit signed, 11 bit exponent, 52 bit mantessa == 64 bit , bias value is 1023
+    return 0;
 }
+
+/*Encoding for integers is with 2's complement, 
+unsigned char = 255 CPU will consider it 
+but for further computation the number will be stored in 2's complement form i.e when you do 255+1 will result to 0*/
--- dataTypes/c-asm.s
+++ dataTypes/c-asm.s
@@ -13,10 +13,26 @@ decode:
 	addi	s0,sp,32
 	li	a5,-1
 	sw	a5,-20(s0)
-	lw	a5,-20(s0)
+	lui	a5,%hi(.LC0)
+	lw	a5,%lo(.LC0)(a5)
+	sw	a5,-24(s0)
+	lui	a5,%hi(.LC1)
+	lw	a4,%lo(.LC1)(a5)
+	lw	a5,%lo(.LC1+4)(a5)
+	sw	a4,-32(s0)
+	sw	a5,-28(s0)
+	li	a5,0
 	mv	a0,a5
 	lw	s0,28(sp)
 	addi	sp,sp,32
 	jr	ra
 	.size	decode, .-decode
+	.section	.rodata
+	.align	2
+.LC0:
+	.word	1075838976
+	.align	3
+.LC1:
+	.word	0
+	.word	1074003968
 	.ident	"GCC: () 9.3.0"
